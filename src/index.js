120aba73797049c3fd2b1f0c6d9dd28ae58a66b4
생이(SangE/남상이) <tkddl5990@gmail.com>
2022-02-09 19:14:16 +0900

    feat: storybook init

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import reportWebVitals from "./reportWebVitals";
+import reportWebVitals from "@/reportWebVitals";
 import App from "@layouts/App";
 
 ReactDOM.render(
